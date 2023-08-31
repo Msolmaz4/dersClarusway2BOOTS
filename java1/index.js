6c084a96c211c85bc29c6e0b05d77ba375d4037e
mustafa <msolmaz83@gmail.com>
2023-08-31 22:35:52 +0200

    maqu

---
+++ java1/index.js
@@ -0,0 +1,69 @@
+console.log('hello motor')
+// fdfdsf
+// ddsdsf
+// dgdgdsg
+// gdgdgprompt('adoiiz')
+document.write('browserde sonuc goserme')
+
+///----------dgiskenler 
+
+//const degisken bir kere girlir ve degistirduginde hata verir 
+//let a = prompt('sayi giriniy')
+
+// for(i=0 ; i<a ; i++){
+//     let b = i
+//     let c =b*b
+//   console.log(c,'op')
+// }
+
+// let er = [6,23,34]
+// let der = [6,5,6,45]
+// console.log(er[0])
+// const ere = ()=>{
+//   if(der[0]== 6 || der[der.length-1] == 6) {
+//  console.log('sonuc hatali')
+//   } else{ console.log('sonuc dogru')}
+ 
+
+// }
+// ere()
+
+
+// //Math 
+// const pi = Math.PI
+// console.log(pi)
+
+// console.log(Math.round(pi))
+
+// //Math.pow
+// //pow(taban,us)
+// console.log(Math.pow(5,3))
+// console.log(5**3)
+// //Math.sqrt
+// console.log(Math.sqrt(49))
+//bactik
+//contact 
+let ada = [1,4,4,9,3]
+ let ar = ada[ada.length-1]
+ console.log(typeof ar)
+
+ let sonuc = ada.push('5')
+ console.log(sonuc)
+//console.log(ada[0] +ada[1])
+//et er = ada.push(7)
+//console.log(ada,'er')
+let d = []
+console.log(d,'ilk d')
+for(i= 0 ; i<ada.length - 1 ;i++){
+  console.log(i)
+  d.push(Number([i]-[i]))
+}
+
+
+
+ console.log(d)
+
+
+
+ 
+ 
