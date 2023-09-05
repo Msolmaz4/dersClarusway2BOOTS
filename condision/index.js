4336a165a1c11676b42a8def91e3e90bddc161bf
mustafa <msolmaz83@gmail.com>
2023-09-05 15:16:45 +0200

    switsch_case

---
+++ condision/index.js
@@ -0,0 +1,36 @@
+// //kitsp turu girince kitamoner
+// let a =prompt('kitap turu').toLocaleLowerCase()
+
+
+
+
+
+// switch (a) {
+//     case 'novel':
+//         console.log(`${a} Anna Karennina`)
+//         break;
+//     case 'fiction':
+//         console.log(`${a} Notre Dame`)
+//         break;
+//     case'poerty':
+//         console.log(`${a} Pillow Thouht`)
+//         break;
+
+//     default:
+//         break;
+// }
+///////////////////////////////////////
+///////////////////////////////////////
+// condtion ? (sart dogru ) : (baba gidik) es6 ile geldi
+//////////////////////////////
+///////////////////////////////
+
+const age =-20
+
+console.log(age>= 0 ? 'poz ' : 'neg')
+ 
+
+operator = '+'
+const num = 145
+const num2 = 67
+console.log( operator === '+' ? 'num+num2' : 'num-num2')
