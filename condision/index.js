a01200b7cae9da484d41fb7b244cc5b166599afc
mustafa <msolmaz83@gmail.com>
2023-09-05 16:57:40 +0200

    case

--- condision/index.js
+++ condision/index.js
@@ -25,12 +25,42 @@
 //////////////////////////////
 ///////////////////////////////
 
-const age =-20
+// const age =-20
 
-console.log(age>= 0 ? 'poz ' : 'neg')
+// console.log(age>= 0 ? 'poz ' : 'neg')
  
 
-operator = '+'
-const num = 145
-const num2 = 67
-console.log( operator === '+' ? 'num+num2' : 'num-num2')
+// operator = '+'
+// const num = 145
+// const num2 = 67
+// console.log( operator === '+' ? 'num+num2' : 'num-num2')
+
+// let ada = +prompt('deger gitirniz')
+
+// if(ada %10 === 0){
+//     console.log(`${ada*100} kazanilan bonus`)
+// }
+// else if(ada%5 === 0){
+//     console.log(`${ada*5} kazanilan bonus`)
+// }
+// else if(ada%4 === 0){
+//     console.log(`${ada*3} kazanilan bonus`)
+// }
+// else{
+//     console.log('avucunu yala')
+// }
+
+// let ada = prompt('easya turunu yaniyz')
+
+
+// switch(ada){
+//     case 'ekmek':
+//         console.log('gida copune') 
+//         break
+//     case 'sise':
+//         console.log('cam copne')
+//         default :
+//         break
+
+// }
+
