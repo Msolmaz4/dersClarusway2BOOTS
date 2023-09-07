afc86cb891e66dd374f512465cb293f1f786119f
mustafa <msolmaz83@gmail.com>
2023-09-07 22:26:43 +0200

    while

--- condision/index.js
+++ condision/index.js
@@ -88,15 +88,146 @@
 //     console.log(ortlama)
     
 // (ortlama)
-function add(a, b) {
-    const dere = a+b
-    console.log(dere)
-   return dere
-}
-add(3 ,5)
+// function add(a, b) {
+//     const dere = a+b
+//     console.log(dere)
+//    return dere
+// }
+// add(3 ,5)
 
-for(i= 1 ; i<5 ; i++){
-    for(j=1 ; j<5 ; j++){
-        console.log(i*j)
-    }
+// for(i= 1 ; i<5 ; i++){
+//     for(j=1 ; j<5 ; j++){
+//         console.log(i*j)
+//     }
+// }
+
+// const mesaj = "merhaba <br>"
+
+
+// for(i= 1 ; i<5 ; i++){
+//     for(j= 1 ; j <= i ;j++){
+//         document.write(mesaj)
+//     }
+     
+// }
+// let  deren = [3,5]
+//  for(i=2 ;i<100 ;i++){ 
+//      if(i%2 != 0 && i%3 !=0 && i%4 != 0 && i%5 != 0 && i%7 != 0)
+//     {
+//         console.log(i,'gelen ilk deger')
+//         deren.push(i)
+//         console.log(deren,'deren')
+//      }
+    
+//}
+
+// let gen = 20
+// let yuk = 10
+
+// for(i= 0 ;i<yuk ;i++){
+    
+    
+// }
+
+// let a = [1,2,3,4,4,4,1,2,5]*-
+// let b= []
+// for(i=0; i<a.length ;i++){
+//     if(a[i] != a.includes(a[i])){
+//         b.push(a[i])
+//         console.log(b)
+//     }
+//     else console.log('denemeler')
+// }
+
+//1 de n 1000 
+// let art = 0
+
+// for(i = 2 ; i < 100 ; i+=2){
+//     art += i   
+// }
+//  console.log(art)
+
+
+//  let gt = 6 
+// let bg = 1
+
+
+//  for(i= 1 ; i<=gt ; i++){
+//     bg*=i
+    
+//  }console.log(bg,'bg')
+// let n = 85
+
+// for(i = 2; i <n ;i++){
+//     let isAsal = true
+//     for(let j = 2 ; j<i ;j++){
+//         if(i%j === 0 ){
+//             isAsal =false
+//             break
+//         }
+//         if(isAsal){
+//            console.log(`${i} asal bir sayidir`)
+//         }
+//     }
+// } 
+
+
+/////////////amstrong 
+// let po = prompt('bitr sayi giriniz')
+ 
+// let a= po.split('')
+// console.log(a,'girilen deger')
+// let b =0
+// for(i=0 ; i < a.length ;i++){
+//     if( b =Number(a[i]**3) ) {
+//         console.log(b,'ilk gelem')
+//         b+=b
+//         console.log(b,'b+=b')
+//         console.log(po)
+//         console.log(typeof b)
+//         console.log(Number(b) == Number(po) ? 'okey ' : 'no')
+//     }
+  
+// }
+// let sdf = -5
+// let fds = 9
+// let sonuc=0
+// for(i=sdf; i<=fds ; i++){
+//  sonuc+=i
+//     console.log(sonuc)
+// }
+//let dere =Math.floor(Math.random()*100)
+// let dere=69
+// console.log(typeof dere)
+// let por = +prompt('deger giriniz')
+// console.log(typeof por)
+
+
+// for(i = 0 ; i<5 ;i++){
+//     console.log(i)
+
+// if( dere == por) {
+//     alert('kazandiniz')
+// }
+// else if(dere > por){
+//     prompt('buyuk  sayi giriniz')
+    
+// }
+// else {
+//     alert('kicik sayi giriniz')
+// }
+
+// }
+
+
+
+//let dere =Math.floor(Math.random()*100)
+ let dere=69
+// console.log(typeof dere)
+ let por = +prompt('deger giriniz')
+// console.log(typeof por)
+
+for(i = 0 ; i<5 ; i++){
+
 }
+
