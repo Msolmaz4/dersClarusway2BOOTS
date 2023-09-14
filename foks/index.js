ea0933c767493ae1e8750e251b600100bb6f5d51
mustafa <msolmaz83@gmail.com>
2023-09-14 23:10:48 +0200

    soru

--- foks/index.js
+++ foks/index.js
@@ -34,3 +34,41 @@ console.log(dre(35,15,12))
 const mu = (fiyat)=> fiyat + (fiyat*18)/100
  
 console.log(mu(100))
+
+function evenNumber (num){
+    return console.log(num)
+    }
+    evenNumber(3) // output : 3 is odd
+    evenNumber(2)
+
+
+let deger = +prompt('kelvin bur deger giriniz')
+
+const sou = (gelen)=>{
+    let fgh =  (gelen -273.15).toFixed(2)
+    return  fgh
+}
+console.log(sou(deger))
+
+
+
+
+ const ciko =(k,b,g) => g < k*2 +b*5 ?  (console.log('okey')) : (console.log('hayir'))
+ ciko(10,5,89)
+
+
+
+
+  
+  let tarih = new Date().getDate(); 
+  console.log(tarih)
+  const tat = (tarih)=>{
+ if(tarih %7 == 0 && tarih %7 == 6){
+   return  console.log('tatil')
+  }
+  else{
+   return  console.log('dersvar')
+  }
+  }
+ 
+tat(tarih)
