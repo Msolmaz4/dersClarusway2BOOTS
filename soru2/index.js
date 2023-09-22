d9238b88e4ecf687f6ca612a5be15d4f364b23fb
mustafa <msolmaz83@gmail.com>
2023-09-22 11:36:45 +0200

    while

--- soru2/index.js
+++ soru2/index.js
@@ -1,38 +1,85 @@
-console.log('sorular2');
+// console.log('sorular2');
 
-const dene = 'assak;lsdbv;dkslbv;sdlev'
-const ses = 'aeioAEIüÜ'
-const der= []
-for(i = 0;i<dene.length ; i++){
-    if(ses.includes(dene[i])){
-          console.log('okey'); 
-          der.push(dene[i])
-          console.log(der);
+// const dene = 'assak;lsdbv;dkslbv;sdlev'
+// const ses = 'aeioAEIüÜ'
+// const der= []
+// for(i = 0;i<dene.length ; i++){
+//     if(ses.includes(dene[i])){
+//           console.log('okey'); 
+//           der.push(dene[i])
+//           console.log(der);
+//     }
+//  else{
+// console.log('hatali');
+// }
+// }
+
+
+
+
+
+
+//  const dem = [ 12,14,100,18,89,90,400]
+// // dem.sort((a , b) => a -b )
+// // console.log (dem[dem.length-1])
+//  let son = dem[0]
+// for(i = 0; i<dem.length ;i++){
+   
+//     if(dem[i] > son){
+//         son = dem[i]
+//     }
+// }
+// console.log(son);
+
+
+
+// const dert = [23,34,45]
+// let sonuc = 0
+// for(i=0 ; i<dert.length ; i++){
+//     sonuc += dert[i]
+// }
+// console.log(sonuc);
+
+
+
+
+
+// const fgt = [45,89,89,100]
+// let son1 = 0
+// for(i =0 ; i<fgt.length ;i++){
+//     son1 += fgt[i]
+// }
+// console.log(son1/fgt.length);
+
+
+
+// const dft = 'dfgg klfkldl vlckv'
+// console.log(dft.split(' ').length);
+
+// let sent = 'javas guyel bir dil'
+// console.log(sent.split(' ').reverse().join(' '));
+
+
+
+
+
+const isim = []
+
+while(true){
+    
+    const derwn = prompt('isim gir')
+    if(derwn === 'q')
+    {
+        isim.pop()
+        break
     }
  else{
-console.log('hatali');
-}
-}
- const dem = [ 12,14,100,18,89,90,400]
-// dem.sort((a , b) => a -b )
-// console.log (dem[dem.length-1])
+     isim.push(derwn)
 
- let son = dem[0]
-////////////
-for(i = 0; i<dem.length ;i++){
-   
-    if(dem[i] > son){
-        son = dem[i]
-       
-    }
-    
+ }   
+  
 }
-console.log(son);
+ console.log(isim);
 
-const dert = [23,34,45]
-let sonuc = 0
 
-for(i=0 ; i<dert.length ; i++){
-    sonuc += dert[i]
-}
-console.log(sonuc);
+
